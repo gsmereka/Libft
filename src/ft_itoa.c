7dc8008feda784b08313a9617cd2fc5c4471a62b
gsmereka <gabrielsmereka200@gmail.com>
2022-12-24 17:38:55 -0300

    fix itoa 2

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 21:55:25 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/12/24 17:28:14 by gsmereka         ###   ########.fr       */
+/*   Updated: 2022/12/24 17:36:14 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/libft.h"
 
-static size_t	ft_calculate_size(int n);
+static size_t	ft_calculate_str_size(int n, int is_negative);
 static char		*set_str(int is_negative, size_t str_size, int n);
 
 char	*ft_itoa(int n)
@@ -29,11 +29,7 @@ char	*ft_itoa(int n)
 		is_negative = 1;
 	else
 		is_negative = 0;
-	if (is_negative)
-		n = n * (-1);
-	str_size = ft_calculate_size(n);
-	if (is_negative)
-		str_size++;
+	str_size = ft_calculate_str_size(n, is_negative);
 	str = set_str(is_negative, str_size, n);
 	return (str);
 }
@@ -43,14 +39,16 @@ static char	*set_str(int is_negative, size_t str_size, int n)
 	size_t		limit;
 	char		*str;
 
+	limit = 0;
 	str = (char *)ft_calloc((str_size + 1), sizeof(char));
 	if (!str)
 		return (NULL);
-	limit = 0;
 	if (is_negative)
+	{
 		limit = 1;
-	if (is_negative)
 		str[0] = '-';
+		n = n * (-1);
+	}
 	while (str_size > limit)
 	{
 		str[str_size - 1] = (n % 10) + '0';
@@ -60,15 +58,17 @@ static char	*set_str(int is_negative, size_t str_size, int n)
 	return (str);
 }
 
-static size_t	ft_calculate_size(int n)
+static size_t	ft_calculate_str_size(int n, int is_negative)
 {
-	size_t	i;
+	size_t	str_size;
 
-	i = 0;
+	str_size = 0;
 	while (n != 0)
 	{
 		n = n / 10;
-		i++;
+		str_size++;
 	}
-	return (i);
+	if (is_negative)
+		str_size++;
+	return (str_size);
 }
